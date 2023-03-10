b43963ac6bcda5322f6a07141ef6cca6096e9a23
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-11 01:58:50 +0500

    create cat to test work

---
+++ src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+    public static void main(String[] args) {
+        Pet cat1 = new Cat();
+        System.out.print(cat1);
+        System.out.println(cat1.voice());
+        System.out.println(cat1.weasel());
+    }
+}
