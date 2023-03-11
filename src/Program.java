7e4dc82dbec16b7f81f993bbdc6d05431577d91d
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-11 16:07:18 +0500

    test zoo info

--- src/Program.java
+++ src/Program.java
@@ -1,17 +1,22 @@
+import Animals.Zoo;
+
+import java.util.Scanner;
+
 public class Program {
     public static void main(String[] args) {
-        /*Pet cat1 = new Cat();
-        System.out.print(cat1);
-        System.out.println(cat1.voice());
-        System.out.println(cat1.weasel());*/
+        Scanner input = new Scanner(System.in);
+        Zoo zoo = new Zoo();
+        zoo.zooInfo();
+    }
 
-
-        Pet dog = new Dog();
-        System.out.println(dog);
-        System.out.println(dog.train());
-        System.out.println(dog.train());
-        System.out.println(dog.train());
-        System.out.println(dog.train());
-        System.out.println(dog);
+    private static String showMenu() {
+        return  "1. Добавить животное в зоопарк\n" +
+                "2. Удалить животное из зоопарка\n" +
+                "3. Просмотреть информацию о животном\n" +
+                "4. Заставить животное издать звук\n" +
+                "5. Просмотреть информацию зоопарке\n" +
+                "6. Заставить зоопарк издавать звуки\n" +
+                "7. Выйти\n" +
+                "Выберите пункт меню: ";
     }
 }
