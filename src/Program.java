e5e4d6d2daa246f21cb09ea3478792abe5d9b878
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-03-11 13:57:29 +0500

    test work with the dog

--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,17 @@
 public class Program {
     public static void main(String[] args) {
-        Pet cat1 = new Cat();
+        /*Pet cat1 = new Cat();
         System.out.print(cat1);
         System.out.println(cat1.voice());
-        System.out.println(cat1.weasel());
+        System.out.println(cat1.weasel());*/
+
+
+        Pet dog = new Dog();
+        System.out.println(dog);
+        System.out.println(dog.train());
+        System.out.println(dog.train());
+        System.out.println(dog.train());
+        System.out.println(dog.train());
+        System.out.println(dog);
     }
 }
